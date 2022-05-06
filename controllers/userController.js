b4d37a01f860146fb8d5a01ec6e8bad05971d75c
pdhende <pdhendework@gmail.com>
2022-05-05 20:35:19 -0400

    Created route to get a single user by ID

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,11 +3,24 @@ const { User, Thought } = require('../models');
 
 module.exports = {
     // method to get all users
-    getUsers(req, res){
-        // console.log("inside get route");
+    getUsers(req, res) {
         User.find({})
-        .select('-__v')
-        .then((users) => res.json(users))
-        .catch((err) => res.status(500).json(err));
+            .select('-__v')
+            .then((users) => res.json(users))
+            .catch((err) => res.status(500).json(err));
     },
+
+    // method to get a single user based on Id
+    getUserById(req, res) {
+        User.findById({
+            _id: req.params.id
+        }).select('-__v')
+        .then((user) => {
+            if(!user){
+                res.status(404).json({ message: 'User not found!'});
+            }else{
+                res.json(user);
+            }
+        }).catch((err) => res.status(500).json(err));
+    }
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
-const { getUsers } = require('../../controllers/userController');
+const { getUsers, getUserById } = require('../../controllers/userController');
 
 router.route('/').get(getUsers);
+router.route('/:id').get(getUserById);
 
 module.exports = router;
